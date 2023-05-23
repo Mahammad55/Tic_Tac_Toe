fa99f98e1d72b9efed661a696f5ce541b08727ce
Mehemmed <ilyazovmehemmed@gmail.com>
2023-05-24 02:11:57 +0400

    added print board method

--- src/Method.java
+++ src/Method.java
@@ -1,2 +1,10 @@
 public class Method {
+    public static void printBoard(char [][] gameBoard){
+        for(char [] board: gameBoard){
+            for(char c:board){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
 }
